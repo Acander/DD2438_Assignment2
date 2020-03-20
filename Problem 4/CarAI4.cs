974255402da123fe33ee77b447bf6e36685b3e38
Acander <adrianz@kth.se>
2020-03-20 01:46:59 +0100

    semi-functional solution

---
+++ Problem 4/CarAI4.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Numerics;
+using Scrips;
+using Vector3 = UnityEngine.Vector3;
+
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof(CarController))]
+    public class CarAI4 : MonoBehaviour
+    {
+        private CarController m_Car; // the car controller we want to use
+        private float car_radius = 5f;
+
+        private double inner_height = 5;
+        private double outer_height = 10;
+        private double inner_width = 2;
+        private double outer_width = 4;
+        private float standardAcceleration = 0.1f;
+        private GameObject parent;
+
+        private CarFormation vShape;
+        private CarFormation sweepShape;
+        private CarFormation currentFormation;
+
+        public GameObject terrain_manager_game_object;
+        TerrainManager terrain_manager;
+        private TraversabilityManager traversabilityManager;
+
+        public GameObject[] friends;
+        public GameObject[] enemies;
+
+        private void Start()
+        {
+            // get the car controller
+            m_Car = GetComponent<CarController>();
+            terrain_manager = terrain_manager_game_object.GetComponent<TerrainManager>();
+            traversabilityManager = new TraversabilityManager(terrain_manager, car_radius);
+
+
+            // note that both arrays will have holes when objects are destroyed
+            // but for initial planning they should work
+            friends = GameObject.FindGameObjectsWithTag("Player");
+            enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            
+            
+            //Init V-formation 
+            
+            //Car 1 (Inner left)
+            CarPosition innerLeft = new CarPosition(friends[0].transform.position, new Vector3(-8, 0,  -8));
+
+            //Car 2 (Inner right)
+            CarPosition innerRight = new CarPosition(friends[0].transform.position, new Vector3(8, 0, -8));
+            
+            //Car 3 (Outer left)
+            CarPosition outerLeft = new CarPosition(friends[0].transform.position, new Vector3(-16, 0, -16));
+            
+            //Car 4 (Outer right)
+            CarPosition outerRight = new CarPosition(friends[0].transform.position, new Vector3(16, 0, -16));
+            
+            vShape = new CarFormation(friends, innerLeft, innerRight, outerLeft, outerRight);
+
+            //Init Sweep-formation
+            //Car 1 (Inner left)
+            innerLeft = new CarPosition(friends[0].transform.position, new Vector3(-16, 0,  0));
+
+            //Car 2 (Inner right)
+            innerRight = new CarPosition(friends[0].transform.position, new Vector3(16, 0,  0));
+            
+            //Car 3 (Outer left)
+            outerLeft = new CarPosition(friends[0].transform.position, new Vector3(-32, 0,  0));
+            
+            //Car 4 (Outer right)
+            outerRight = new CarPosition(friends[0].transform.position, new Vector3(32, 0,  0));
+            
+            sweepShape = new CarFormation(friends, innerLeft, innerRight, outerLeft, outerRight);
+
+            currentFormation = vShape;
+        }
+        
+        
+        private void FixedUpdate()
+        {
+
+            CarControls carControls = currentFormation.getCarControls(gameObject);
+            
+            m_Car.Move(carControls.steering, carControls.acceleration, carControls.acceleration, 0f);
+
+
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.cyan;
+            Debug.Log(currentFormation.innerLeft.position);
+            Gizmos.DrawSphere(currentFormation.innerLeft.position, 3f);
+            Gizmos.DrawSphere(currentFormation.innerRight.position, 3f);
+            Gizmos.DrawSphere(currentFormation.outerLeft.position, 3f);
+            Gizmos.DrawSphere(currentFormation.outerRight.position, 3f);
+        }
+    }
+}
---
+++ Problem 4/CarFormation.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityStandardAssets.Vehicles.Car;
+
+namespace Scrips
+{
+    //Formation class for 4 cars (not including a replay/leader car)
+    
+    public class CarFormation
+    {
+        //Parameters calculated as dot products based on the leaders position
+        
+        //Leader
+        private GameObject leader;
+        private CarPosition lastLeaderPosition;
+        private Vector3 oldForwardVector;
+        
+        //Other Cars
+        private GameObject[] friends;
+        
+        //Car 1 (Inner left)
+        public CarPosition innerLeft;
+
+        //Car 2 (Inner right)
+        public CarPosition innerRight;
+        
+        //Car 3 (Outer left)
+        public CarPosition outerLeft;
+        
+        //Car 4 (Outer right)
+        public CarPosition outerRight;
+
+        public CarFormation(GameObject[] friends, CarPosition innerLeft, CarPosition innerRight, CarPosition outerLeft, CarPosition outerRight)
+        {
+            leader = friends[0];
+            lastLeaderPosition = new CarPosition(leader.transform.position, new Vector3(0, 0, 0));
+            oldForwardVector = leader.transform.forward;
+            Debug.LogFormat("Initial position {0}", lastLeaderPosition.position);
+            this.innerLeft = innerLeft;
+            this.innerRight = innerRight;
+            this.outerLeft = outerLeft;
+            this.outerRight = outerRight;
+            this.friends = friends;
+        }
+
+
+        public CarControls getCarControls(GameObject car)
+        {
+            //Vector3 update = leader.transform.position - lastLeaderPosition.position;
+            float dot = Vector3.Dot(leader.transform.right, oldForwardVector);
+            float dir = dot > 0f ? -1f : 1f;
+            float angle = dir * Vector3.Angle(oldForwardVector, leader.transform.forward);
+            Vector3 leaderPosition = leader.transform.position; 
+            Debug.LogFormat("Update angle: {0}", angle);
+            
+            //Save old positions
+            innerLeft.oldPos = innerLeft.position;
+            innerRight.oldPos = innerRight.position;
+            outerLeft.oldPos = outerLeft.position;
+            outerRight.oldPos = outerRight.position;
+            
+            innerLeft.differenceVector = Quaternion.Euler(0, angle, 0) * innerLeft.differenceVector;
+            innerLeft.position = leaderPosition - innerLeft.differenceVector;;
+            innerRight.differenceVector = Quaternion.Euler(0, angle, 0) * innerRight.differenceVector;
+            innerRight.position = leaderPosition - innerRight.differenceVector;
+            outerLeft.differenceVector = Quaternion.Euler(0, angle, 0) * outerLeft.differenceVector;
+            outerLeft.position = leaderPosition - outerLeft.differenceVector;
+            outerRight.differenceVector = Quaternion.Euler(0, angle, 0) * outerRight.differenceVector;
+            outerRight.position = leaderPosition - outerRight.differenceVector;
+            
+            lastLeaderPosition.position = leader.transform.position;
+            oldForwardVector = leader.transform.forward;
+
+            /*foreach(var friend in friends)
+            {
+                parentPosition += friend.transform.position;
+            }
+
+            parentPosition = parentPosition / friends.Length;*/
+            
+            /*if (isInnerCar(car))
+            {
+                parentPosition = leader.transform.position;
+            }
+            else if (friends[3].name == car.name)
+            {
+                leader = friends[1];
+                parentPosition = friends[1].transform.position;
+            }
+            else if (friends[4].name == car.name)
+            {
+                leader = friends[2];
+                parentPosition = friends[2].transform.position;
+            }
+            
+            myPosition = car.transform.position;
+            Debug.DrawLine(myPosition, parentPosition);
+            dirToLeader = myPosition - parentPosition;
+            //dirNorm = dirToLeader.normalized;*/
+
+            Vector3 myPosition = car.transform.position;
+            bool passed = false;
+            bool is_to_the_right = true;
+            //bool tooFarAway = false;
+            
+            //double steeringError 
+
+            if (friends[1].name == car.name)
+            {
+                //Left
+                passed = this.passed(car.transform.position, innerLeft.oldPos, innerLeft.position);
+                is_to_the_right = Vector3.Dot(innerLeft.position - car.transform.position, car.transform.right) < 0;
+                Debug.DrawLine(innerLeft.position, car.transform.position);
+            } 
+            else if (friends[3].name == car.name)
+            {
+                passed = this.passed(car.transform.position, outerLeft.oldPos, outerLeft.position);
+                is_to_the_right = Vector3.Dot(outerLeft.position - car.transform.position, car.transform.right) < 0;
+                Debug.DrawLine(outerLeft.position, car.transform.position);
+            }
+            else if (friends[2].name == car.name)
+            {
+                passed = this.passed(car.transform.position, innerRight.oldPos, innerRight.position);
+                is_to_the_right = Vector3.Dot(innerRight.position - car.transform.position, car.transform.right) < 0;
+                Debug.DrawLine(innerRight.position, car.transform.position);
+            }
+            else if (friends[4].name == car.name)
+            {
+                passed = this.passed(car.transform.position, outerRight.oldPos, outerRight.position);
+                is_to_the_right = Vector3.Dot(outerRight.position - car.transform.position, car.transform.right) < 0;
+                Debug.DrawLine(outerRight.position, car.transform.position);
+            }
+            
+            float steering;
+            float acceleration = 0.8f;
+
+            if (is_to_the_right)
+            {
+                steering = -1f;
+            }
+            else
+            {
+                steering = 1f;
+            }
+            /*if (passed)
+            {
+                acceleration = -1f;
+            }
+            else
+            {
+                acceleration = 1f;
+            }*/
+
+            CarControls controls = new CarControls(steering, acceleration, acceleration, 0);
+            return controls;
+        }
+
+        private bool isInnerCar(GameObject car)
+        {
+            return friends[1].name == car.name || friends[2].name == car.name;
+        }
+        
+        private bool passed(Vector3 pos, Vector3 start_pos, Vector3 end_pos)
+        {
+            var between = end_pos - start_pos;
+            var progress = pos - start_pos;
+            var projection = Vector3.Project(progress, between);
+            
+            return projection.magnitude >= between.magnitude;
+        }
+
+    }
+
+    public class CarPosition
+    {
+        //Old solution
+        //Optimal Scalar product values
+        //public double distanceFromLeader;
+        //public double angleProduct;
+        //public double angle;
+        
+        //New Solution
+        //Projection magnitude
+        //public double max_height;
+        //public double min_height;
+        //public double max_width;
+        //public double min_width;
+        public Vector3 position;
+        public Vector3 differenceVector;
+        public Vector3 oldPos;
+        public Vector3 currentPos;
+        
+        public CarPosition(Vector3 leaderPosition, Vector3 difference)
+        {
+            /*this.max_height = max_height;
+            this.min_height = min_height;
+            this.angle = angle;
+            this.angleProduct = angleProduct;*/
+
+            position = leaderPosition + difference;
+            differenceVector = -difference;
+            oldPos = position;
+        }
+    }
+
+    public class CarControls
+    {
+        public float steering;
+        public float acceleration;
+        public float footBrake;
+        public float handBrake;
+
+        public CarControls(float steering, float acceleration, float footBrake, float handBrake)
+        {
+            this.steering = steering;
+            this.acceleration = acceleration;
+            this.footBrake = footBrake;
+            this.handBrake = handBrake;
+        }
+    }
+    
+    
+    
+}
